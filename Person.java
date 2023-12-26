795a55c2e3ab06eb23c61615eb72f1e30ac99d8a
YeYintPhyoeAung <yeyintphyoeaung23@gmail.com>
2023-12-26 15:27:44 +0630

    Self-study java lessons

---
+++ Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    
+    public static void main(String[] args) {
+        System.out.println("------------------");
+        System.out.println("----We are JDC----");
+        System.out.println("------------------");
+
+        
+    }
+}
