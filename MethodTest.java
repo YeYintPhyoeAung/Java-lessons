92c20d0a2751bd9a8b9aac936dfc1cdc64892275
Asta <152241492+YeYintPhyoeAung@users.noreply.github.com>
2023-12-27 12:06:15 +0630

    Update MethodTest.java
    Adding new changes and making result

--- MethodTest.java
+++ MethodTest.java
@@ -1,10 +1,24 @@
 public class MethodTest {
 
     public static void main(String[] args) {
+        sayHello();
 
+        showAddResult(10, 5);
+
+        int result = add(10, 10);
+
+        System.out.println(result);
     }
 
     static void showAddResult(int a, int b) {
         System.out.println(a+b);
+    }
+
+    static void sayHello() {
+        System.out.println("Hello");
+    }
+
+    staic int add(int a, int b) {
+        return a + b;
     }
 }
