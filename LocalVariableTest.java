ffbf99178e30152bc62d06d864bf0e95a65e7dfc
Asta <152241492+YeYintPhyoeAung@users.noreply.github.com>
2023-12-27 11:39:28 +0630

    Method-learning

---
+++ LocalVariableTest.java
@@ -0,0 +1,10 @@
+public class LocalVariableTest {
+    
+    public static void showMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void doSomething() {
+        String message = "Doing Something";
+    }
+}
---
+++ MethodTest.java
@@ -0,0 +1,10 @@
+public class MethodTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    static void showAddResult(int a, int b) {
+        System.out.println(a+b);
+    }
+}
